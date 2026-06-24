b0777f4e03c6d341c53b41ff2c90daf804426578
UROBBYU <srobby228@gmail.com>
2026-06-24 20:47:23 +0300

    Fix package tarball filename in packaging script

--- pack.mjs
+++ pack.mjs
@@ -7,5 +7,5 @@ delete pkg.devDependencies
 
 sh.exec('npm run build')
 fs.writeFileSync('./dist/package.json', JSON.stringify(pkg, null, 2))
-sh.mv(sh.exec('npm pack ./dist').trim(), './pkg.tgz')
+sh.mv(sh.exec('npm pack ./dist').trim(), './package.tgz')
 sh.exec('npm run clear')
